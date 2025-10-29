3acd5c069ff240513f9792c85f9ed9e764d37722
Joona Kupe <joona.kupe@helsinki.fi>
2025-10-29 09:44:27 +0200

    use Asap as a global font

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -2,6 +2,8 @@ const styles = {
   /**
    * root wraps the whole app and these are therefore
    * the default styles
+   * Using fonts as per e-kirjasto visual guide
+   * Joona Kupe 2025
    */
   root: {
     fontFamily: "body",
@@ -9,7 +11,12 @@ const styles = {
     fontSize: 0,
     lineHeight: 3,
     color: "ui.black"
-  }
+  },
+  fonts: {
+    body: "Asap,sans-serif",
+    heading: "Asap,sans-serif",
+    monospace: "Menlo, monospace"
+  },
 };
 
 export default styles;
