945c5197eede8deac26de09fd7664a41c2020d23
Joona Kupe <joona.kupe@helsinki.fi>
2026-02-06 12:32:19 +0200

    run extract script

--- src/pages/[library]/test/index.tsx
+++ src/pages/[library]/test/index.tsx
@@ -1,26 +1,26 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { GetStaticPaths, GetStaticProps } from "next";
+import React from "react";
 
 export default function TestPage({ _locale }: { _locale: string }) {
-    const { t } = useTranslation("common");
-  
-    return (
-      <main style={{ padding: 40 }}>
-        <p>Locale: {_locale}</p>
-        <h1>{t("title")}</h1>
-        <p>{t("description")}</p>
-      </main>
-    );
-  }
-  
+  const { t } = useTranslation("common");
+
+  return (
+    <main style={{ padding: 40 }}>
+      <p>Locale: {_locale}</p>
+      <h1>{t("title")}</h1>
+      <p>{t("description")}</p>
+    </main>
+  );
+}
 
 export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
   const libraries = ["ekirjasto"];
 
   const paths =
-    locales?.flatMap((locale) =>
-      libraries.map((library) => ({
+    locales?.flatMap(locale =>
+      libraries.map(library => ({
         params: { library },
         locale
       }))
@@ -33,13 +33,11 @@ export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-    const currentLocale = locale ?? "en";
-  
-    return {
-      props: {
-        ...(await serverSideTranslations(currentLocale, ["common"])),
-        _locale: currentLocale
-      }
-    };
+  const currentLocale = locale ?? "en";
+  return {
+    props: {
+      ...(await serverSideTranslations(currentLocale, ["common"])),
+      _locale: currentLocale
+    }
   };
-  
+};
