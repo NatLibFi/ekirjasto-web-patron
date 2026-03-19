79ce96236c593b71617c6af23aafd392b1a53e4d
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2026-03-19 22:02:34 +0200

    EKIRJASTO-681 Extract strings from storeBadges components

--- src/components/storeBadges/GooglePlayBadge.tsx
+++ src/components/storeBadges/GooglePlayBadge.tsx
@@ -4,19 +4,22 @@
 // @ts-nocheck
 import { jsx } from "theme-ui";
 import * as React from "react";
+import { useTranslation } from "next-i18next";
 
 const GooglePlayBadge = (props: React.ComponentProps<"a">) => {
+  const { t } = useTranslation();
+
   return (
     <a
       rel="noopener noreferrer"
       target="__blank"
-      aria-label="Get E-kirjasto on the Google Play Store"
+      aria-label={t("storeBadges.ariaLabelForPlayStoreLink")}
       href="https://play.google.com/store/apps/details?id=fi.kansalliskirjasto.ekirjasto"
       sx={{ display: "block" }}
       {...props}
     >
       <img
-        alt="Get it on Google Play"
+        alt={t("storeBadges.altForPlayStoreImage")}
         sx={{ width: "100%" }}
         src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
       />
