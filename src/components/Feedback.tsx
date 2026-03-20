6bd3c668e42c8b71e74503427d71b0029da34fdc
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2026-03-20 13:46:40 +0200

    EKIR-775 Receive CSRF token postMessages from trusted source

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -3,7 +3,31 @@ import useLibraryContext from "./context/LibraryContext";
 
 const Feedback: React.FC = () => {
   const iframeRef = useRef<HTMLIFrameElement>(null);
+  const library = useLibraryContext();
 
+  useEffect(() => {
+    function handleMessage(event: MessageEvent) {
+      console.debug(
+        "[Feedback] Received postMessage:",
+        event.data,
+        "from",
+        event.origin
+      );
+      // Only messages from trusted source are accespted
+      if (
+        event.data &&
+        event.data.type === "feedback-csrf-token" &&
+        typeof event.data.csrfToken === "string" &&
+        event.origin === library.feedbackUrl
+      ) {
+        console.debug("[Feedback] CSRF token received:", event.data.csrfToken);
+      }
+    }
+    window.addEventListener("message", handleMessage);
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, [library.feedbackUrl]);
 
   return (
     <div
@@ -21,6 +45,7 @@ const Feedback: React.FC = () => {
       <iframe
         ref={iframeRef}
         src="https://lib.e-kirjasto.fi/palaute/"
+        src={library.feedbackUrl || undefined}
         title="Feedback"
         width="90%"
         height="100%"
