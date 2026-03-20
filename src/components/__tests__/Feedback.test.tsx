4f76c0474ffd0eba382831350eb48affa5c6d70e
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2026-03-20 17:35:33 +0200

    EKIR-775 Update tests

--- src/components/__tests__/Feedback.test.tsx
+++ src/components/__tests__/Feedback.test.tsx
@@ -1,13 +1,71 @@
 import React from "react";
-import { render, screen } from "test-utils";
+import { render, screen, fixtures } from "test-utils";
 import Feedback from "../Feedback";
+import merge from "deepmerge";
+import { LibraryData } from "interfaces";
 
-describe("Feedback component", () => {
-  it("renders the iframe feedback page with correct src", () => {
-    render(<Feedback />);
+const library: LibraryData = merge(fixtures.libraryData, {
+  feedbackUrl: "https://test.fi/palaute/"
+});
 
-    const iframe = screen.getByTitle("Feedback");
-    expect(iframe).toBeInTheDocument();
-    expect(iframe).toHaveAttribute("src", "https://lib.e-kirjasto.fi/palaute/");
+test("renders the iframe feedback page with correct src", () => {
+  render(<Feedback />, {
+    library: library
   });
+
+  const iframe = screen.getByTitle("Feedback");
+  expect(iframe).toBeInTheDocument();
+  expect(iframe).toHaveAttribute("src", "https://test.fi/palaute/");
+});
+
+test("accepts postMessage with CSRF token from trusted origin", () => {
+  render(<Feedback />, {
+    library: library
+  });
+
+  // Spy on console.debug
+  const debugSpy = jest.spyOn(console, "debug");
+
+  // Mock the postMessage event
+  const messageEvent = new MessageEvent("message", {
+    data: {
+      type: "feedback-csrf-token",
+      csrfToken: "test-csrf-token"
+    },
+    origin: "https://test.fi/palaute/"
+  });
+  window.dispatchEvent(messageEvent);
+
+  expect(debugSpy).toHaveBeenCalledWith(
+    "[Feedback] CSRF token received:",
+    "test-csrf-token"
+  );
+
+  debugSpy.mockRestore();
+});
+
+test("rejects postMessage with CSRF token from untrusted origin", () => {
+  render(<Feedback />, {
+    library: library
+  });
+
+  // Spy on console.debug
+  const debugSpy = jest.spyOn(console, "debug");
+
+  // Mock the postMessage event from an untrusted origin
+  const messageEvent = new MessageEvent("message", {
+    data: {
+      type: "feedback-csrf-token",
+      csrfToken: "test-csrf-token"
+    },
+    origin: "https://ei-oikea-testi.fi/palaute/"
+  });
+  window.dispatchEvent(messageEvent);
+
+  expect(debugSpy).not.toHaveBeenCalledWith(
+    "[Feedback] CSRF token received:",
+    "test-csrf-token"
+  );
+
+  debugSpy.mockRestore();
 });
