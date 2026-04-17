b63db20dca2aa0aff050d6cc1456a82c9114246c
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2026-04-17 14:25:28 +0300

    EKIRJASTO-816 Redirect to homepage if only one library is available

--- src/components/MultiLibraryHome.tsx
+++ src/components/MultiLibraryHome.tsx
@@ -1,27 +1,87 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, ThemeUIProvider } from "theme-ui";
-import { Themed } from "@theme-ui/mdx";
 import * as React from "react";
-import Link from "next/link";
 import { APP_CONFIG } from "utils/env";
-import theme from "theme/theme";
+import { jsx, ThemeUIProvider } from "theme-ui";
+import { NextRouter, useRouter } from "next/router";
+import { PageLoader } from "components/LoadingIndicator";
 import { useTranslation } from "next-i18next";
+import { Themed } from "@theme-ui/mdx";
+import Link from "next/link";
+import theme from "theme/theme";
 
+/**
+ * Handles the app library routing logic
+ * based on the number of available libraries.
+ *
+ * - if there is only one library => user is redirected to that library's frontpage
+ * - if there are no libraries => "No libraries available" is rendered
+ * - if multiple libraries are available =>  a selection list of libraries is rendered
+ */
 const MultiLibraryHome: React.FC = () => {
   const { libraries, instanceName } = APP_CONFIG;
-  const slugs = Object.keys(libraries);
+  const slugs = Object.keys(libraries ?? {});
   const { t } = useTranslation();
+  const router = useRouter();
+
+  // define state to handle showing loading indicator
+  // set initial state to loading=true
+  const [loading, setLoading] = React.useState(true);
+
+  // effect that calls handleLibraryRouting function
+  // to handle loading state and the app's routing logic
+  // based on the number of libraries (slugs) available
+  React.useEffect(() => {
+    handleLibraryRouting(slugs, router, setLoading);
+  }, [slugs, router]);
+
+  // show loading indicator while fetching data
+  if (loading) return <PageLoader />;
 
   // just in case check if we have no libraries
   // and show user the "No libraries available" page
   // (this should not happen for E-kirjasto)
-  if (!libraries || slugs.length === 0) {
+  if (slugs.length === 0) {
     return renderNoLibraries(instanceName, t);
   }
 
+  // show user the library selection list
+  // (this should not happen for E-kirjasto because
+  // the user should already be redirected to the E-kirjasto frontpage)
   return renderLibrarySelection(instanceName, t, slugs, libraries);
+};
+
+// helper function that updates the loading state
+// and also handles redirection in the case
+// that there is only one library
+const handleLibraryRouting = (
+  slugs: string[],
+  router: NextRouter,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  // check the special case:
+  // if only one library is found
+  if (slugs.length === 1) {
+    // one library found!
+    // note: this should be the E-kirjasto case
+
+    // first get the slug
+    // note: this should be "ekirjasto"
+    const singleLibrarySlug = slugs[0];
+
+    // then redirect the user to the frontpage
+    // of this single library
+    // note: this is the "/ekirjasto" route
+    router.replace(`/${singleLibrarySlug}`);
+
+    // and return early after redirection
+    return;
+  }
+
+  // all other cases (we have no libraries found
+  // or we have multiple libraries):
+  // just stop the loading state
+  setLoading(false);
 };
 
 // helper function that renders the "No libraries available"
