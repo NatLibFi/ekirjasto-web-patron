f6a1a3782d585aa7b0749c2580aae3f85ed41618
Sade-Tuuli Siipola <164179420+natlibfi-burger@users.noreply.github.com>
2026-08-17 10:49:44 +0300

    Make changes to beta banner amd add translations (#148)

--- src/components/BetaBanner.tsx
+++ src/components/BetaBanner.tsx
@@ -26,30 +26,76 @@ const BetaBanner: React.FC<BetaBannerProps> = () => {
 
   // define info texts for beta banner
   const welcomeText = t("betaBanner.infoWelcome");
-  const materialsText =
-    t("betaBanner.infoEbooksAndMagazines") +
-    " " +
-    t("betaBanner.infoAudiobooks");
+  const infoEbooksAndMagazinesText = t("betaBanner.infoEbooksAndMagazines");
+  const infoAudiobooksText = t("betaBanner.infoAudiobooks");
+
   const ekirjastoText = t("betaBanner.infoEkirjasto");
   const ariaLabelForInfoEkirjasto =
     t("betaBanner.infoEkirjasto") + " " + t("externalLink.opensInNewTab");
 
+  const webText = t("betaBanner.infoWeb");
+  const ariaLabelForInfoWeb =
+    t("betaBanner.infoWeb") + " " + t("externalLink.opensInNewTab");
+
+  const androidText = t("betaBanner.infoAndroid");
+  const ariaLabelForInfoAndroid =
+    t("betaBanner.infoAndroid") + " " + t("externalLink.opensInNewTab");
+
+  const iosText = t("betaBanner.infoIos");
+  const ariaLabelForInfoIos =
+    t("betaBanner.infoIos") + " " + t("externalLink.opensInNewTab");
+
   // define external link url for E-kirjasto info
   const hrefForInfoEkirjasto = t("betaBanner.hrefInfoEkirjasto");
+  const hrefForInfoWeb = t("betaBanner.hrefInfoWeb");
+  const hrefForInfoAndroid = t("betaBanner.hrefInfoAndroid");
+  const hrefForInfoIos = t("betaBanner.hrefInfoIos");
 
   return (
     <Stack direction="column" sx={stackStyle}>
       <H3>{welcomeText}</H3>
-      <Text>{materialsText}</Text>
-      <a
-        href={hrefForInfoEkirjasto}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label={ariaLabelForInfoEkirjasto}
-      >
-        <Text>{ekirjastoText}</Text>
-        <ExternalLinkIcon sx={{ ml: 1, fill: "#0576d3" }} />
-      </a>
+      <Text>{infoEbooksAndMagazinesText}</Text>
+      <Text>{infoAudiobooksText}</Text>
+      <Stack direction="row">
+        <a
+          href={hrefForInfoEkirjasto}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={ariaLabelForInfoEkirjasto}
+        >
+          <Text>{ekirjastoText}</Text>
+          <ExternalLinkIcon sx={{ ml: 1, fill: "#0576d3" }} />
+        </a>
+        <a
+          href={hrefForInfoWeb}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={ariaLabelForInfoWeb}
+        >
+          <Text>{webText}</Text>
+          <ExternalLinkIcon sx={{ ml: 1, fill: "#0576d3" }} />
+        </a>
+      </Stack>
+      <Stack direction="row">
+        <a
+          href={hrefForInfoAndroid}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={ariaLabelForInfoAndroid}
+        >
+          <Text>{androidText}</Text>
+          <ExternalLinkIcon sx={{ ml: 1, fill: "#0576d3" }} />
+        </a>
+        <a
+          href={hrefForInfoIos}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={ariaLabelForInfoIos}
+        >
+          <Text>{iosText}</Text>
+          <ExternalLinkIcon sx={{ ml: 1, fill: "#0576d3" }} />
+        </a>
+      </Stack>
     </Stack>
   );
 };
